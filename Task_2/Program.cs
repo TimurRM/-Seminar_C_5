82ca0e9025bf4d62356468eb47725e8ac0f23996
Timur <tumyp.paxumob@gmail.com>
2022-07-13 23:30:52 +0500

    Added tasks

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье  число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a < (b + c) && b < (a + c) && c < (a + b)){
+    Console.WriteLine("да");
+}
+else{
+    Console.WriteLine("нет");
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,60 @@
+﻿
+Console.Write("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+            
+        int q = 0; 
+        int i = 0;
+                        
+       
+       
+        int[] newArray = new int[100];
+ 
+            while (number >= 1)
+            {
+                q = number % 2;
+                newArray[i] = q;
+                i++;
+ 
+                number /=  2;
+                Console.Write(q);
+            }
+        Console.Write(" -> ");
+
+int[] ReversArray(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int j = 0; j < inArray.Length; j++)
+    {
+        result[j] = inArray[inArray.Length - 1 - j];
+    }
+    
+    return result;
+}
+int[] reversArray = ReversArray(newArray);
+Console.WriteLine(String.Join("", reversArray));
+
+// void ReversArray1(int[] inArray)
+// {
+//     for (int i = 0; i < inArray.Length / 2; i++)
+//     {
+//         int k = inArray[i];
+//         inArray[i] = inArray[inArray.Length - i - 1];
+//         inArray[inArray.Length - i - 1] = k;
+//     }
+// }
+
+// ReversArray1(newArray);
+// Console.WriteLine(String.Join("", newArray));
+
+Console.Write("Десятичное число: ");
+int dec = Convert.ToInt32(Console.ReadLine());
+
+    string bin = string.Empty;
+            while (dec > 0)
+            {
+                bin = dec % 2 + bin;
+                dec /= 2;
+            }
+          
+            Console.WriteLine($"Двоичное число: {bin}");
+
