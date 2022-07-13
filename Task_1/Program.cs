d293d2b78f3c781a785d09ca51bbcb86092ab674
Timur <tumyp.paxumob@gmail.com>
2022-07-13 23:34:01 +0500

    Compare

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,45 +2,51 @@
 // Напишите программу, которая перевернёт одномерный массив 
 //(последний элемент будет на первом месте, а первый - на последнем и т.д.)
 
-int[] GetArray(int size, int minValue, int maxValue)
+internal class Program
 {
-    int[] res = new int[size];
-
-    for (int i = 0; i < size; i++)
+    private static void Main(string[] args)
     {
-        res[i] = new Random().Next(minValue, maxValue + 1);
+        int[] GetArray(int size, int minValue, int maxValue)
+        {
+            int[] res = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                res[i] = new Random().Next(minValue, maxValue + 1);
+            }
+            return res;
+        }
+
+        void ReversArray1(int[] inArray)
+        {
+            for (int i = 0; i < inArray.Length / 2; i++)
+            {
+                int k = inArray[i];
+                inArray[i] = inArray[inArray.Length - i - 1];
+                inArray[inArray.Length - i - 1] = k;
+            }
+        }
+
+        int[] ReversArray2(int[] inArray)
+        {
+            int[] result = new int[inArray.Length];
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                result[i] = inArray[inArray.Length - 1 - i];
+            }
+            return result;
+        }
+
+
+        Console.Clear();
+
+        int[] array = GetArray(10, 0, 10);
+        Console.WriteLine(string.Join(" ", array));
+
+        int[] reversArray = ReversArray2(array);
+        Console.WriteLine(string.Join(" ", reversArray));
+
+        ReversArray1(array);
+        Console.WriteLine(string.Join(" ", array));
     }
-    return res;
 }
-
-void ReversArray1(int[] inArray)
-{
-    for (int i = 0; i < inArray.Length / 2; i++)
-    {
-        int k = inArray[i];
-        inArray[i] = inArray[inArray.Length - i - 1];
-        inArray[inArray.Length - i - 1] = k;
-    }
-}
-
-int[] ReversArray2(int[] inArray)
-{
-    int[] result = new int[inArray.Length];
-    for (int i = 0; i < inArray.Length; i++)
-    {
-        result[i] = inArray[inArray.Length - 1 - i];
-    }
-    return result;
-}
-
-
-Console.Clear();
-
-int[] array = GetArray(10, 0, 10);
-Console.WriteLine(String.Join(" ", array));
-
-int[] reversArray=ReversArray2(array);
-Console.WriteLine(String.Join(" ", reversArray));
-
-ReversArray1(array);
-Console.WriteLine(String.Join(" ", array));
--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,13 +1,21 @@
-﻿Console.WriteLine("Введите первое число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b= Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите третье  число: ");
-int c = Convert.ToInt32(Console.ReadLine());
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите третье  число: ");
+        int c = Convert.ToInt32(Console.ReadLine());
 
-if(a < (b + c) && b < (a + c) && c < (a + b)){
-    Console.WriteLine("да");
-}
-else{
-    Console.WriteLine("нет");
+        if (a < b + c && b < a + c && c < a + b)
+        {
+            Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
+    }
 }
